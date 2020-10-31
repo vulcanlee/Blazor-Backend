4dbf678388b749325d8fede02d8d0e713d74fd00
Vulcan <vulcan.lee@gmail.com>
2020-10-31 21:38:08 +0800

    建立資料庫專案與EF Entity Model

---
+++ Src/Backend/DataAccessLayer/Models/Order.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DataAccessLayer.Models
+{
+    public partial class Order
+    {
+        public Order()
+        {
+            OrderItem = new HashSet<OrderItem>();
+        }
+
+        public int OrderId { get; set; }
+        public byte Status { get; set; }
+        public DateTime OrderDate { get; set; }
+        public DateTime RequiredDate { get; set; }
+        public DateTime? ShippedDate { get; set; }
+
+        public virtual ICollection<OrderItem> OrderItem { get; set; }
+    }
+}
---
+++ Src/Backend/DataAccessLayer/Models/Product.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DataAccessLayer.Models
+{
+    public partial class Product
+    {
+        public Product()
+        {
+            OrderItem = new HashSet<OrderItem>();
+        }
+
+        public int ProductId { get; set; }
+        public string Name { get; set; }
+        public short ModelYear { get; set; }
+        public decimal ListPrice { get; set; }
+
+        public virtual ICollection<OrderItem> OrderItem { get; set; }
+    }
+}
